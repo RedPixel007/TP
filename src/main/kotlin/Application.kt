d9ca2455c04c13773d9fb789fa6b3b176ea3d939
Trofi <0pixart0@gmailc.om>
2026-05-12 15:55:15 +0300

    Refactoring-change jvm version

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,12 +1,15 @@
 package com.example
 
 import io.ktor.server.application.*
+import io.ktor.server.netty.*
+import io.ktor.server.engine.*
+import com.example.plugins.configureRouting
 
-fun main(args: Array<String>) {
-    io.ktor.server.netty.EngineMain.main(args)
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
 }
 
 fun Application.module() {
-    configureSerialization()
     configureRouting()
 }
--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -1,15 +1,21 @@
-package com.example
+package com.example.plugins
 
-import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
-import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     routing {
+        post("/compute") {
+            call.respondText("OK: задача принята (заглушка)")
+        }
+
+        get("/result/{id}") {
+            val id = call.parameters["id"] ?: "unknown"
+            call.respondText("OK: результат для id=$id (заглушка)")
+        }
         get("/") {
-            call.respondText("Hello World!")
+            call.respondText("Сервер работает!")
         }
     }
 }
